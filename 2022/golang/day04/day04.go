b51a11e874c846ba05b510276ce4454e47908a95
Ezequiel Barbero <barbero.oe@gmail.com>
2023-10-08 14:24:50 -0300

    Day04

--- 2022/golang/day04/day04.go
+++ 2022/golang/day04/day04.go
@@ -1,5 +1,62 @@
 package day04
 
+import (
+	"strconv"
+	"strings"
+)
+
 func PartOne(lines []string) int {
-	return 0
+	total := 0
+	for _, line := range lines {
+		first, second := parse(line)
+		if isContained(first, second) || isContained(second, first) {
+			total++
+		}
+	}
+	return total
+}
+
+func PartTwo(lines []string) int {
+	total := 0
+	for _, line := range lines {
+		first, second := parse(line)
+		if overlaps(first, second) || overlaps(second, first) {
+			total++
+		}
+	}
+	return total
+}
+
+type Range struct {
+	Start int
+	End   int
+}
+
+func parse(line string) (Range, Range) {
+	assignments := strings.Split(line, ",")
+	first := createRange(assignments[0])
+	second := createRange(assignments[1])
+	return first, second
+}
+
+func createRange(assignment string) Range {
+	r := strings.Split(assignment, "-")
+	start, err := strconv.Atoi(r[0])
+	if err != nil {
+		panic(err)
+	}
+	end, err := strconv.Atoi(r[1])
+	if err != nil {
+		panic(err)
+	}
+	return Range{start, end}
+}
+
+func isContained(r1, r2 Range) bool {
+	return r1.Start <= r2.Start && r2.End <= r1.End
+}
+
+func overlaps(r1, r2 Range) bool {
+	return r1.Start <= r2.Start && r2.Start <= r1.End ||
+		r1.Start <= r2.End && r2.End <= r1.End
 }
--- 2022/golang/day04/day04_test.go
+++ 2022/golang/day04/day04_test.go
@@ -9,5 +9,8 @@ import (
 func TestDay04(t *testing.T) {
 	RunAoCTest(t, []TestCase[AoC[int]]{
 		{Name: "01-Test", Tc: AoC[int]{Filename: "test", Expected: 2, Fn: PartOne}},
+		{Name: "01-Real", Tc: AoC[int]{Filename: "input", Expected: 580, Fn: PartOne}},
+		{Name: "02-Test", Tc: AoC[int]{Filename: "test", Expected: 4, Fn: PartTwo}},
+		{Name: "02-Real", Tc: AoC[int]{Filename: "input", Expected: 895, Fn: PartTwo}},
 	})
 }
